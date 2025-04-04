b619ce668930ab7c4a9ae7c7481fb00b581dd1ae
artem <babkin.artem@gmail.com>
2025-04-04 16:56:04 +0500

    upd

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#curl 'https://bfmeladder.com/api/replays' > tmp/replays.json.tmp
+curl 'https://bfmeladder.com/api/replays' > tmp/replays.json.tmp
 python3 gather_stats.py 'tmp/replays.json.tmp' 'src/data/ennorath_data.json' 'Ennorath' '1.9.7.3' '8a0e14c4-3204-4973-96e3-62ba9dc8526b'
 rm -rf docs/*
 npm run build
