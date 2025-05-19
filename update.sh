004c09ab21f085720726a460e21b166fbd206db6
artem <babkin.artem@gmail.com>
2025-05-19 11:25:12 +0500

    upd

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo 'Downloading replay list'
-#curl 'https://bfmeladder.com/api/replays' > tmp/replays.json.tmp
-echo 'gather 1.9.7.3 stats'
-python3 gather_stats.py 'tmp/replays.json.tmp' 'src/data/ennorath_data_1973.json' 'Ennorath' '1.9.7.3' '8a0e14c4-3204-4973-96e3-62ba9dc8526b'
+curl 'https://bfmeladder.com/api/replays' > tmp/replays.json.tmp
+#echo 'gather 1.9.7.3 stats'
+#python3 gather_stats.py 'tmp/replays.json.tmp' 'src/data/ennorath_data_1973.json' 'Ennorath' '1.9.7.3' '8a0e14c4-3204-4973-96e3-62ba9dc8526b'
 echo 'gather 1.9.7.5 stats'
 python3 gather_stats.py 'tmp/replays.json.tmp' 'src/data/ennorath_data_1975.json' 'Ennorath' '1.9.7.5' '8a0e14c4-3204-4973-96e3-62ba9dc8526b'
+#echo 'gather 1.9.7.3 matchaps'
+#python3 legacy_matchaps.py 'tmp/replays.json.tmp' 'src/matchaps1973.html' 'Ennorath' '1.9.7.3'
 echo 'gather 1.9.7.5 matchaps'
-python3 legacy_matchaps.py 'tmp/replays.json.tmp' 'src/matchaps1973.html' 'Ennorath' '1.9.7.3'
-echo 'gather 1.9.7.3 matchaps'
 python3 legacy_matchaps.py 'tmp/replays.json.tmp' 'src/matchaps1975.html' 'Ennorath' '1.9.7.5'
 rm -rf docs/*
 echo 'building'
