3877513a6168529a8bf7ad4cd94b3f50d6503345
artem <babkin.artem@gmail.com>
2025-06-11 15:32:11 +0500

    upd

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Downloading replay list'
-curl 'https://bfmeladder.com/api/replays' > tmp/replays.json.tmp
+#curl 'https://bfmeladder.com/api/replays' > tmp/replays.json.tmp
 #echo 'gather 1.9.7.3 stats'
 #python3 gather_stats.py 'tmp/replays.json.tmp' 'src/data/ennorath_data_1973.json' 'Ennorath' '1.9.7.3' '8a0e14c4-3204-4973-96e3-62ba9dc8526b'
 echo 'gather 1.9.7.5 stats'
